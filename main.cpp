299f12e1112db023429662b736b88f1cb42582cd
Tim Amsdon <42815488+tamsdon@users.noreply.github.com>
2022-02-14 15:53:28 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
-/*  Moving point
-*   Draws a point at the centre of the screen, its position changes
-*   in accordance with user input (via the joystick).
+/*  Program responds to joystick movement and displays real-time, 
+*   mapped coordinates on the LCD and reports them to serial comms port
 *
-*   Author: Andrew Knowles 2021
+*   Author: Dr Tim Amsdon Feb 2022
 */
 
 #include "mbed.h"
@@ -15,11 +14,6 @@ Joystick joystick(PC_3, PC_2);  //attach and create joystick object
 //Pin assignment format:  lcd(IO, Ser_TX, Ser_RX, MOSI, SCLK, PWM)  
 N5110 lcd(PC_7, PA_9, PB_10, PB_5, PB_3, PA_10);
 
-//set initial position of point to (42,24)
-float y_pos = 24;
-float x_pos = 42;
-
-void boundary(int x, int y);
 
 int main(){
     joystick.init();
@@ -28,70 +22,18 @@ int main(){
     lcd.setBrightness(0.5);     //set brightness to 50% (utilises the PWM)
     
     while (1) {
-        lcd.clear();
-        lcd.drawRect(0,0,84,48,FILL_TRANSPARENT);   //draws screen boarder
-        lcd.drawRect(x_pos,y_pos,2,2,FILL_BLACK);   //draws point at position (x,y)
-
-        /*  read the direction of the joystick and create new co-ordinates for the point.
-        *   y-- : decrement the y co-ordinate of the point thus moving it towards the top of the screen 
-        *   x-- : decrement the x co-ordinate of the point thus moving it towards the left hand side of the screen 
-        */
-
-        if(joystick.get_direction() == N){
-            y_pos--;
-            printf(" Direction: N\n");
-
-        }else if(joystick.get_direction() == S){
-            y_pos++;
-            printf(" Direction: S\n");
+          // read the joystick to get the x- and y- values
+        Vector2D coord = joystick.get_mapped_coord(); 
+        printf("Coord = %f | %f\n",coord.x,coord.y);    
         
-        }else if(joystick.get_direction() == E){
-            x_pos++;
-            printf(" Direction: E\n");
-
-        }else if(joystick.get_direction() == W){
-            x_pos--;
-            printf(" Direction: W\n");
-
-        }else if(joystick.get_direction() == NE){
-            y_pos--;
-            x_pos++;
-            printf(" Direction: NE\n");
-
-        }else if(joystick.get_direction() == NW){
-            y_pos--;
-            x_pos--;
-            printf(" Direction: NW\n");
-
-        }else if(joystick.get_direction() == SE){
-            y_pos++;
-            x_pos++;
-            printf(" Direction: SE\n");
-
-        }else if(joystick.get_direction() == SW){
-            y_pos++;
-            x_pos--;
-            printf(" Direction: SW\n");
-        }
-
-        boundary(x_pos, y_pos); //pass the new x,y position to function
-            
-        lcd.refresh();
-        ThisThread::sleep_for(30ms);
-    }
-}
-
-//check that the point remains on the screen
-void boundary(int x, int y){
-    if (x < 1 ){        //ensure that the point doesnt pass beyond the left hand side of the screen
-        x_pos = 1;
-    }else if (x > 81){  //ensure that the point doesnt pass beyond the right hand side of the screen-
-        x_pos = 81;
-    }
-
-    if(y < 1){          //ensure that the point doesnt pass beyond the top of the screen
-        y_pos = 1;
-    }else if(y > 45){   //ensure that the point doesnt pass beyond the bottom of the screen
-        y_pos = 45;
+        lcd.clear();  // clear buffer at the start of the loop
+        char buffer[14]={0};  // each character is 6 pixels wide, screen is 84 pixels (84/6 = 14)
+        sprintf(buffer,"x = %.3f",coord.x); // print formatted data to buffer
+        lcd.printString(buffer,0,2);     // display on screen
+        sprintf(buffer,"y = %.3f",coord.y); // print formatted data to buffer
+        lcd.printString(buffer,0,3);     // display on screen
+        lcd.refresh();  // need to fresh the screen to get the message to appear
+        
+        thread_sleep_for(30ms);
     }
 }
